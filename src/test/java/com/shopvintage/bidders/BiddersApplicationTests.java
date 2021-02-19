f293904dfdd5911e5e5baadc5b0916c9b5158958
Brynden Rios <sir.brynden.rios@gmail.com>
2021-02-19 18:16:20 -0500

    version inicial

---
+++ src/test/java/com/shopvintage/bidders/BiddersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopvintage.bidders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
